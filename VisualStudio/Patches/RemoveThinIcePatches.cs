919d5c244aa61fca667ff7117bbb14bf42bdb907
Sm4rtBoyTom <155878804+Sm4rtBoyTom@users.noreply.github.com>
2025-09-28 12:46:41 +0200

    Add files via upload

---
+++ VisualStudio/Patches/RemoveThinIcePatches.cs
@@ -0,0 +1,17 @@
+﻿using HUDImprovements;
+
+[HarmonyPatch(typeof(Panel_HUD), nameof(Panel_HUD.Update))]
+internal class ThinIcePatches
+{
+    internal static void Postfix(ref Panel_HUD __instance)
+    {
+        if (Settings.instance.ThinIce == true)
+        {
+            __instance.m_ThinIceWidget.gameObject.SetActive(true);
+        }
+        else
+        {
+            __instance.m_ThinIceWidget.gameObject.SetActive(false);
+        }
+    }
+}
